15ea8ad3acc5076d706078fa04ad3a234eb0502f
21stPhenom <phenom.h.acks21@gmail.com>
2023-10-03 13:25:13 +0100

    created user and shopItem models

---
+++ schema/shopItems.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const shopItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    isInStock: {
+        type: Boolean,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }
+}, {timestamps: true});
+
+const shopItems = mongoose.model('shopitems', shopItemSchema);
+
+module.exports = shopItems;
